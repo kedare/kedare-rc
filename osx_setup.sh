c41ce6d6d2e1712c4a56b4a0885ad8eb8a165da2
Mathieu Poussin <mathieu.poussin@netyxia.net>
2016-12-31 16:31:58 +0100

    Add setup script for osx

---
+++ osx_setup.sh
@@ -0,0 +1,26 @@
+
+
+# Install development tools
+xcode-select --install
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install powerlines fonts
+
+git clone --depth=1 https://github.com/powerline/fonts.git
+cd fonts
+bash install.sh
+cd
+
+# Installing development tools
+brew cask install java visual-studio-code emacs
+brew install git sonar-scanner vim emacs go python3 postgresql mongodb elixir erlang influxdb consul nodejs
+brew install neovim/neovim/neovim
+
+# Install operation tools
+brew install mtr ipcalc sipcalc minicom
+
+# Install common tools
+brew cask install vlc
+brew install wine
